411e766c3e8abf4573affc3b08cc160756f136fc
unknown <ogbonnaiheoma48@gmail.com>
2022-07-02 13:29:08 +0100

    now

--- Server.js
+++ Server.js
@@ -6,9 +6,9 @@ const path = require("./router/BusinessRouter");
 
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("Welcome to FoodAviz API");
-});
+// app.get("/", (req, res) => {
+//   res.send("Welcome to FoodAviz API");
+// });
 
 app.use("/api", path);
 
