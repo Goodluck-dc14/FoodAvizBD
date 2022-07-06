bf1405a557c8f7b6511705881f6e228c9a18481e
unknown <ogbonnaiheoma48@gmail.com>
2022-07-06 14:28:07 +0100

    db changes

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 port = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI;
+const uri = process.env.URI;
 
 mongoose
-  .connect(MONGODB_URI)
+  .connect(uri)
   .then(() => {
     console.log("database connected...");
   })
